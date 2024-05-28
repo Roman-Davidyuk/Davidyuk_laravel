14ab53f49bbe2e44c703e2da0ef3979d51483089
Roman-Davidyuk <roman.davydiuk@oa.edu.ua>
2024-05-28 17:59:32 +0300

    lab6

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -7,41 +7,42 @@ use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
 use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Repositories\BlogCategoryRepository;
 
 class CategoryController extends BaseController
 {
     /**
-     * Display a listing of the resource.
+     * @var BlogCategoryRepository
      */
+    private $blogCategoryRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
+
     public function index()
     {
-        $paginator = BlogCategory::paginate(5);
-
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
         return view('blog.admin.categories.index', compact('paginator'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $item = new BlogCategory();
-        $categoryList = BlogCategory::all();
-
+        $categoryList = $this->blogCategoryRepository->getForComboBox(); // Змінна $categoryList має бути оголошена
         return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(BlogCategoryCreateRequest $request)
     {
-        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        $data = $request->input();
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
         }
 
-        $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
+        $item = (new BlogCategory())->create($data);
 
         if ($item) {
             return redirect()
@@ -54,44 +55,36 @@ class CategoryController extends BaseController
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        // Тут реалізуйте логіку, якщо потрібно
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        $item = BlogCategory::findOrFail($id);
-        $categoryList = BlogCategory::all();
-
+        $item = $this->blogCategoryRepository->getEdit($id);
+        if (empty($item)) {
+            abort(404);
+        }
+        $categoryList = $this->blogCategoryRepository->getForComboBox($item->parent_id);
         return view('blog.admin.categories.edit', compact('item', 'categoryList'));
-
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-        $item = BlogCategory::find($id);
-        if (empty($item)) { //якщо ід не знайдено
-            return back() //redirect back
-            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
-            ->withInput(); //повернути дані
+        $item = $this->blogCategoryRepository->getEdit($id); // Отримуємо об'єкт з репозиторію
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
+                ->withInput();
         }
 
-        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        $data = $request->all();
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
         }
 
-        $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
+        $result = $item->update($data);
 
         if ($result) {
             return redirect()
@@ -99,17 +92,15 @@ class CategoryController extends BaseController
                 ->with(['success' => 'Успішно збережено']);
         } else {
             return back()
-                ->with(['msg' => 'Помилка збереження'])
+                ->withErrors(['msg' => 'Помилка збереження'])
                 ->withInput();
         }
-
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        // Тут реалізуйте логіку видалення, якщо потрібно
     }
 }
+
+
